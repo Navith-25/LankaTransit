47694f3776c7e7823673253468d35b1ac9587e70
Navith-25 <wwnavith@gmail.com>
2025-12-17 16:12:46 +0530

    Implement Spring Security

---
+++ src/main/java/com/nibm/lankatransit/AuthController.java
@@ -0,0 +1,26 @@
+package com.nibm.lankatransit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Simple Registration Endpoint
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered: " + user.getUsername();
+    }
+
+    // TODO: In the next step, we will add the /login endpoint that returns a JWT.
+}
